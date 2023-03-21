dafb020bb5f9c69e49a846e4258bfe1878839dc5
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-03-21 17:05:50 +0700

    leoleo

---
+++ test-cases/New test case (11).ks.ts
@@ -0,0 +1,5 @@
+import katalon from "katalon";
+
+export default katalon.testCase("New Test Case", async ({ local, web }) => {
+  await web.openBrowser("https://katalon-demo-cura.herokuapp.com");
+});
