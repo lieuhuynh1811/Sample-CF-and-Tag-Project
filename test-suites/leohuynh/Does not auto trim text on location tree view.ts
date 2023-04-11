e966ba71e53375eda38e3904e8add33a885f92a5
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-04-11 15:25:13 +0700

    commit

---
+++ test-suites/leohuynh/Does not auto trim text on location tree view.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>Does not auto trim text on location tree view</name>
+    <testSuiteGuid>1b8799cb-f5af-4474-9e98-707099c140ce</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/leo</testCaseId>
+        <guid>73166560-dd71-43af-bb4f-ea0ea5a5ba7e</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/test-folder/Test Case 4</testCaseId>
+        <guid>f8b724d5-af10-4448-8333-e807b9f39ed1</guid>
+    </testCaseLink>
+</TestSuiteEntity>
