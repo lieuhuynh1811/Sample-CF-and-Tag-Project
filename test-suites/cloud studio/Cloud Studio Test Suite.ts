8f9d8647ac8fa008fc707ae2fc427939dcb2633d
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-04-11 16:44:27 +0700

    first

---
+++ test-suites/cloud studio/Cloud Studio Test Suite.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>Cloud Studio Test Suite</name>
+    <testSuiteGuid>b883e35b-443a-490c-abf9-dd9e5feebf1a</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/leo</testCaseId>
+        <guid>294bce51-b0dd-4d16-a4f9-5350619709e6</guid>
+    </testCaseLink>
+</TestSuiteEntity>
