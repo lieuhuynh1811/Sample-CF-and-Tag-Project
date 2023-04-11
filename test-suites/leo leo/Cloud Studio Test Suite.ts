b207ede521261873212b29130dd78c02257050ee
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-04-11 15:28:29 +0700

    first commit

---
+++ test-suites/leo leo/Cloud Studio Test Suite.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>Cloud Studio Test Suite</name>
+    <testSuiteGuid>24955158-394b-44b1-a2d9-a876e645b0b7</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/leo</testCaseId>
+        <guid>70afa295-8835-4c32-ab9b-5c34343b0724</guid>
+    </testCaseLink>
+</TestSuiteEntity>
