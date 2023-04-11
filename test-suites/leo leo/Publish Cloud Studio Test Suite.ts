b5c705090ea5657f9e95e0efdc8ba1e73c577051
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-04-11 11:42:58 +0700

    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.

---
+++ test-suites/leo leo/Publish Cloud Studio Test Suite.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>Publish Cloud Studio Test Suite</name>
+    <testSuiteGuid>1ecead67-4cb6-489b-a272-df769736b422</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/leo</testCaseId>
+        <guid>dc759e60-dfbb-4c9b-ab5a-4e357a1833ce</guid>
+    </testCaseLink>
+</TestSuiteEntity>
