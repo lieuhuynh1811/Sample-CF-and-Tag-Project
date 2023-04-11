f2f9132cf8d43bfca5b34e353b91499c5f8839c9
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-04-11 14:44:38 +0700

    first

---
+++ test-suites/leo leo/focus folder.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>focus folder</name>
+    <testSuiteGuid>c8b4e454-c45d-4002-bad2-2236c6dd1485</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/leo</testCaseId>
+        <guid>cc81c527-d049-407e-9626-da6f479d97c9</guid>
+    </testCaseLink>
+</TestSuiteEntity>
