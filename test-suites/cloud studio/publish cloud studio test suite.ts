90b9e0d612f15f33c9716d32178918721ecc3949
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-04-11 11:38:35 +0700

    second

--- test-suites/cloud studio/publish cloud studio test suite.ts
+++ test-suites/cloud studio/publish cloud studio test suite.ts
@@ -1,13 +1,21 @@
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <TestSuiteEntity>
     <name>publish cloud studio test suite</name>
-    <testSuiteGuid>586d98d4-3fd9-4de2-9126-401eeb01a938</testSuiteGuid>
+    <testSuiteGuid>928c8f84-5fa0-45ec-8126-557715da7a0a</testSuiteGuid>
     <testCaseLink>
         <testCaseId>test-cases/leo</testCaseId>
-        <guid>f809e3bf-fcfc-45e5-a7cd-91e430d1f368</guid>
+        <guid>89e615fb-7b5e-42bf-997e-045566da0787</guid>
     </testCaseLink>
     <testCaseLink>
         <testCaseId>test-cases/lieuht96/Verify the user can login</testCaseId>
-        <guid>b504e715-5682-453a-a7b4-d63d95a82d53</guid>
+        <guid>83ff93c0-da4f-48b7-9f8a-a9dee718bb4a</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/Draft Test From Turing Team</testCaseId>
+        <guid>b3fb9a2c-652f-4f22-8a5d-d9ef85327d2b</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/regression/Regression 3</testCaseId>
+        <guid>72836048-3821-4128-837e-90e702e371b5</guid>
     </testCaseLink>
 </TestSuiteEntity>
