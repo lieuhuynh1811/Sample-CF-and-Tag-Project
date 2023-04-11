bb570df98e7e224494441844a129515b893cc056
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-04-11 11:31:39 +0700

    first commit

---
+++ test-suites/cloud studio/publish cloud studio test suite.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>publish cloud studio test suite</name>
+    <testSuiteGuid>586d98d4-3fd9-4de2-9126-401eeb01a938</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/leo</testCaseId>
+        <guid>f809e3bf-fcfc-45e5-a7cd-91e430d1f368</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/Verify the user can login</testCaseId>
+        <guid>b504e715-5682-453a-a7b4-d63d95a82d53</guid>
+    </testCaseLink>
+</TestSuiteEntity>
