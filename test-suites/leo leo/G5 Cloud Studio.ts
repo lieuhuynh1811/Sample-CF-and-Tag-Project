1447fe6d3f549ce2aa26df8764a3d8b24d9197cb
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-04-11 14:55:22 +0700

    first

---
+++ test-suites/leo leo/G5 Cloud Studio.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>G5 Cloud Studio</name>
+    <testSuiteGuid>af3d4660-f6f8-4dae-91d5-5dbb49882e95</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/Draft Test From Turing Team</testCaseId>
+        <guid>96d4f089-d9d8-4384-8a4a-83abf3abb215</guid>
+    </testCaseLink>
+</TestSuiteEntity>
