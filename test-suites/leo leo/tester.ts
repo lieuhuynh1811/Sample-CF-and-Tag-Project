e1c00440a5c9e4fb40ab1fff10cfc879ea07cb71
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-04-11 10:05:05 +0700

    first commit

---
+++ test-suites/leo leo/tester.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>tester</name>
+    <testSuiteGuid>f4ff40ff-9984-423e-830e-2b63c6786716</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/Draft Test From Turing Team</testCaseId>
+        <guid>743f3f08-9d55-483a-92a8-0016baf6ac0f</guid>
+    </testCaseLink>
+</TestSuiteEntity>
