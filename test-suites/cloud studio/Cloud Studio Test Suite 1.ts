86e98b3659698ef4097566b858c8b0e458d34c52
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-04-13 17:22:40 +0700

    Katalon: Update test suite Cloud Studio Test Suite 1 in test-suites/cloud studio

--- test-suites/cloud studio/Cloud Studio Test Suite 1.ts
+++ test-suites/cloud studio/Cloud Studio Test Suite 1.ts
@@ -1,13 +1,17 @@
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <TestSuiteEntity>
     <name>Cloud Studio Test Suite 1</name>
-    <testSuiteGuid>83f64a81-5d2c-4618-b04e-bac5bf38f310</testSuiteGuid>
+    <testSuiteGuid>9d28381f-8297-4f35-8541-e501e1b6bdae</testSuiteGuid>
     <testCaseLink>
         <testCaseId>test-cases/bug fixing/draft test 1</testCaseId>
-        <guid>0e98fc96-835e-4724-9a7e-ec161d526196</guid>
+        <guid>bf4d4a93-337e-4a73-b83b-ddee4e53aeab</guid>
     </testCaseLink>
     <testCaseLink>
         <testCaseId>test-cases/lieuht96/test-folder/G5 Test Case</testCaseId>
-        <guid>fccd7327-a56d-42ca-909b-38a2dad8c7bf</guid>
+        <guid>4cec5591-5080-466e-93e0-f7a350d4ae6d</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/leo</testCaseId>
+        <guid>3bfc55be-8acb-48b2-89cc-a81d35765e8d</guid>
     </testCaseLink>
 </TestSuiteEntity>
