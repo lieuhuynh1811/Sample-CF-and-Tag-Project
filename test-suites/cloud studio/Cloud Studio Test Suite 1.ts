a3c1136a9c54b9f5133f28a28d7a9b88931333e7
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-04-13 17:22:12 +0700

    first

---
+++ test-suites/cloud studio/Cloud Studio Test Suite 1.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>Cloud Studio Test Suite 1</name>
+    <testSuiteGuid>83f64a81-5d2c-4618-b04e-bac5bf38f310</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/bug fixing/draft test 1</testCaseId>
+        <guid>0e98fc96-835e-4724-9a7e-ec161d526196</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/test-folder/G5 Test Case</testCaseId>
+        <guid>fccd7327-a56d-42ca-909b-38a2dad8c7bf</guid>
+    </testCaseLink>
+</TestSuiteEntity>
