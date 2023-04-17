942b0d42103c6bf8c98a7fdb94481a572a5bb351
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-04-17 09:45:11 +0700

    first

---
+++ test-suites/cloud studio/lieuhuynh.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>lieuhuynh</name>
+    <testSuiteGuid>15d116ae-8b43-41a0-976b-1489f97e946a</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/bug fixing/draft test 1</testCaseId>
+        <guid>dfbeedbf-73d5-4d59-92d3-b521fe45b600</guid>
+    </testCaseLink>
+</TestSuiteEntity>
