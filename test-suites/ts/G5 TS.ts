3332d00e5b9372cf71a247ae0722eb998840d328
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-04-25 17:30:01 +0700

    commit from Lieu Huynh

---
+++ test-suites/ts/G5 TS.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>G5 TS</name>
+    <testSuiteGuid>1a26b956-1ce3-4e7e-ab0e-ba67a6ddad58</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuhuynh/New test case (1)</testCaseId>
+        <guid>7c0741ea-739d-48b2-aa3c-ea7407f64405</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuhuynh/New test case (1)</testCaseId>
+        <guid>47317aa1-00ec-4a77-8d90-5b2be95ac13f</guid>
+    </testCaseLink>
+</TestSuiteEntity>
