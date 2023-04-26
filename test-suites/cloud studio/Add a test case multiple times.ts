bb57ca3fa749d9664940373e528cfd4a6fb51aab
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-04-26 14:17:18 +0700

    Add a test case multiple times

---
+++ test-suites/cloud studio/Add a test case multiple times.ts
@@ -0,0 +1,45 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>Add a test case multiple times</name>
+    <testSuiteGuid>e6266a37-e7dc-43cb-8df8-0650d5a6ef2d</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/bug fixing/Lorem Ipsum is simply dummied text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book</testCaseId>
+        <guid>ece2a0ed-da97-4a98-afcf-85ade2f05de6</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/bug fixing/Lorem Ipsum is simply dummied text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book</testCaseId>
+        <guid>38ab3bcc-48cc-470f-9e73-1a5e6a1cb9aa</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/bug fixing/Lorem Ipsum is simply dummied text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book</testCaseId>
+        <guid>4cfbf68e-8bbc-4561-9ac8-483fb6ee9705</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/bug fixing/Lorem Ipsum is simply dummied text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book</testCaseId>
+        <guid>9e47c8d6-bca3-41a2-afd4-f8274c0dc7e3</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/bug fixing/Lorem Ipsum is simply dummied text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book</testCaseId>
+        <guid>0fda9178-70ca-4e50-9c8b-ecdfec039381</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/bug fixing/Lorem Ipsum is simply dummied text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book</testCaseId>
+        <guid>2ab61af2-f420-43ae-8a1d-14549b5e6dbe</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/bug fixing/Lorem Ipsum is simply dummied text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book</testCaseId>
+        <guid>2c303bb8-ad34-484f-88f2-0ea3a94d3fae</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/bug fixing/Lorem Ipsum is simply dummied text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book</testCaseId>
+        <guid>4ff09554-71a2-40ca-a0e5-c294c4e7e84e</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/bug fixing/Lorem Ipsum is simply dummied text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book</testCaseId>
+        <guid>3c23a0f6-0e6d-48b8-9892-c2af65697b0a</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/bug fixing/Lorem Ipsum is simply dummied text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book</testCaseId>
+        <guid>ea033b52-99cc-4c3c-9c30-e360eb10d10d</guid>
+    </testCaseLink>
+</TestSuiteEntity>
