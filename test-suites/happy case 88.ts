7e7f4daa08c5740ba35a10512e2d9d8392701d9a
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-04-26 17:36:32 +0700

    first commit

---
+++ test-suites/happy case 88.ts
@@ -0,0 +1,57 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>happy case 88</name>
+    <testSuiteGuid>b1182317-b572-439a-ab9e-e586e0c6f9b3</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/regression/regression 1/regression 5</testCaseId>
+        <guid>86613b5f-24aa-4cba-b275-e204dbd05a81</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/regression/4.4/regression 4</testCaseId>
+        <guid>9f366e5b-ad81-4792-8f24-b8eeb1f4a8d9</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/regression/Regression 3</testCaseId>
+        <guid>37bb9b6b-eb5f-45cf-abfc-bbd54b067d69</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/test-folder/G5 Test Case</testCaseId>
+        <guid>a09cbc2e-d137-49c1-bea3-ce79c90fba3b</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/test-folder/New test case 1</testCaseId>
+        <guid>1b60ee5f-f1ac-4284-892f-6686ac377546</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/test-folder/Test Case 4</testCaseId>
+        <guid>4a72defd-180c-41a6-9edf-14acf6aa34d5</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/Draft Test From Turing Team</testCaseId>
+        <guid>58a78227-3da8-4521-be15-30d4b878abcc</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/Verify the user can login</testCaseId>
+        <guid>6306b009-748f-4e7f-8340-038109c79876</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/bug fixing/Lorem Ipsum is simply dummied text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book</testCaseId>
+        <guid>ecb85caa-a9c2-4f4c-b3cb-341293c968be</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/bug fixing/draft test 1</testCaseId>
+        <guid>6622c10f-fe9d-49b6-9ca8-a451be3dd229</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/bug fixing/g5</testCaseId>
+        <guid>9d7d05d9-4ebe-442e-937a-e2775e93f8cb</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/New test case (11)</testCaseId>
+        <guid>8a2cbf61-b222-4eb4-a2c2-8239da633c78</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/leo</testCaseId>
+        <guid>e8a27d68-ed71-4b99-bd8f-64e6257c7d92</guid>
+    </testCaseLink>
+</TestSuiteEntity>
