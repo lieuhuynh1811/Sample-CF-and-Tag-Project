24eb2afd1b15c0b6887559fd648d23bb68f90b1b
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-04-26 10:54:58 +0700

    new

---
+++ test-suites/cloud studio/g5.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>g5</name>
+    <testSuiteGuid>40c77b32-a748-433c-bf2d-c27a9d5edd12</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/bug fixing/Lorem Ipsum is simply dummied text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book</testCaseId>
+        <guid>a881d131-6ba4-476f-8c45-8b0feadb26ff</guid>
+    </testCaseLink>
+</TestSuiteEntity>
