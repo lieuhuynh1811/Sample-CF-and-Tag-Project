9f6b5d550ea22a05af3014588eeed1cf24c7bfd7
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-04-26 17:26:17 +0700

    update

---
+++ test-suites/lieu-huynh/test suite 08.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>test suite 08</name>
+    <testSuiteGuid>336ba0c8-4bfd-4b1f-878e-e35516fa7ba2</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/New test case (11)</testCaseId>
+        <guid>060bfedc-b8c6-4b21-bff7-a2e8f6ba1a37</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/leo</testCaseId>
+        <guid>6c068de1-97f2-4de9-b32c-0e445058662e</guid>
+    </testCaseLink>
+</TestSuiteEntity>
