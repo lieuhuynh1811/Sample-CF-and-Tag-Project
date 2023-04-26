558d288547a9b043880e3ce947bd381e97ab51d7
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-04-26 10:12:29 +0700

    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.

--- test-suites/cloud studio/G5 Test Suite.ts
+++ test-suites/cloud studio/G5 Test Suite.ts
@@ -1,45 +1,41 @@
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <TestSuiteEntity>
     <name>G5 Test Suite</name>
-    <testSuiteGuid>74aabe96-eac4-482d-a9ab-4a9894afe3f2</testSuiteGuid>
+    <testSuiteGuid>4c5df983-5af3-4a40-ab6c-ed94f64e7495</testSuiteGuid>
     <testCaseLink>
         <testCaseId>test-cases/regression/regression 1/regression 5</testCaseId>
-        <guid>f2ba7b6a-c7c4-4aca-ad66-1dab95fdf2fb</guid>
+        <guid>3fec0454-d3ee-4670-8899-0d3fe5349022</guid>
     </testCaseLink>
     <testCaseLink>
         <testCaseId>test-cases/regression/4.4/regression 4</testCaseId>
-        <guid>2a5703d3-9869-4157-aeb6-09e13e00a28f</guid>
+        <guid>2692596d-857f-4107-9b34-c518a08503ec</guid>
     </testCaseLink>
     <testCaseLink>
         <testCaseId>test-cases/regression/Regression 3</testCaseId>
-        <guid>87a01f2f-c9bd-459c-a16e-ab8dc89b426a</guid>
+        <guid>71739ee0-2fdc-4995-a7c1-4be0615ba72d</guid>
     </testCaseLink>
     <testCaseLink>
         <testCaseId>test-cases/lieuht96/test-folder/G5 Test Case</testCaseId>
-        <guid>b9d3183e-fec2-4aa5-91de-e2b0532f8344</guid>
+        <guid>cefc851c-2680-436a-9405-cf53e0209f62</guid>
     </testCaseLink>
     <testCaseLink>
         <testCaseId>test-cases/lieuht96/test-folder/New test case 1</testCaseId>
-        <guid>23a95e00-b4f6-476a-8e7f-c04f56f8675a</guid>
+        <guid>e2e358e6-6095-4338-9424-86e4c913b309</guid>
     </testCaseLink>
     <testCaseLink>
         <testCaseId>test-cases/lieuht96/test-folder/Test Case 4</testCaseId>
-        <guid>426a1dd0-258e-4ee4-89be-5fd89fd2f376</guid>
+        <guid>c7df3608-5180-41fd-a090-889f6a28631c</guid>
     </testCaseLink>
     <testCaseLink>
         <testCaseId>test-cases/lieuht96/Draft Test From Turing Team</testCaseId>
-        <guid>3def086e-4349-43d7-b936-144cea7f240d</guid>
+        <guid>bbf416c2-2801-4d5f-965b-7c05ea11b143</guid>
     </testCaseLink>
     <testCaseLink>
         <testCaseId>test-cases/lieuht96/Verify the user can login</testCaseId>
-        <guid>d3ae423a-e1d8-4eca-8431-84755acb3b3a</guid>
+        <guid>29473727-efe2-43d8-84eb-f08d8abf5915</guid>
     </testCaseLink>
     <testCaseLink>
         <testCaseId>test-cases/bug fixing/draft test 1</testCaseId>
-        <guid>76caf2c0-4853-4961-b9f9-c51f0512d62e</guid>
-    </testCaseLink>
-    <testCaseLink>
-        <testCaseId>test-cases/bug fixing/g5</testCaseId>
-        <guid>714900a7-36f6-4800-8398-609eef1ad008</guid>
+        <guid>eef3cec7-c860-4005-9529-a2104238467c</guid>
     </testCaseLink>
 </TestSuiteEntity>
