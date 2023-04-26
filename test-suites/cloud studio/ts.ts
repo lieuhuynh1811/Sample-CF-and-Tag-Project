51e25dd08344e9093c693845142abf7734f60082
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-04-26 17:35:15 +0700

    Katalon: Update test suite ts in test-suites/cloud studio

--- test-suites/cloud studio/ts.ts
+++ test-suites/cloud studio/ts.ts
@@ -1,9 +1,113 @@
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <TestSuiteEntity>
     <name>ts</name>
-    <testSuiteGuid>8d6e7c67-763b-4a95-9747-006bb2a4e12d</testSuiteGuid>
+    <testSuiteGuid>cb34b831-ce35-4709-ac71-c758d2fb87ac</testSuiteGuid>
     <testCaseLink>
         <testCaseId>test-cases/regression/regression 1/regression 5</testCaseId>
-        <guid>3f72c07f-81b1-4022-a204-ce766ce73238</guid>
+        <guid>0a0375c8-3ab0-4e69-80da-0604e5f65268</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/regression/regression 1/regression 5</testCaseId>
+        <guid>ab1f585d-a121-456c-9801-f466d2378a6a</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/regression/4.4/regression 4</testCaseId>
+        <guid>83e2ec51-c089-465b-a949-b493525aa21f</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/regression/Regression 3</testCaseId>
+        <guid>245b4c1c-7b4b-4d99-8a73-af53cc98e11e</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/test-folder/G5 Test Case</testCaseId>
+        <guid>bc819c13-438e-450e-9681-7fc7ea9bd68d</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/test-folder/New test case 1</testCaseId>
+        <guid>7f45ccd1-6a40-4f2d-8727-8d40a4c2e13d</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/test-folder/Test Case 4</testCaseId>
+        <guid>24b3cdfd-148a-4f25-8c5f-655c40854f6c</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/Draft Test From Turing Team</testCaseId>
+        <guid>90f6bad8-04a2-4b2e-a488-3e22ce53038e</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/Verify the user can login</testCaseId>
+        <guid>32912570-f346-45d9-b4fd-cd214a53cabd</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/bug fixing/Lorem Ipsum is simply dummied text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book</testCaseId>
+        <guid>ced03a19-a2c7-48a3-9363-4124c9fd05ac</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/bug fixing/draft test 1</testCaseId>
+        <guid>46f593ca-e3d6-4a7c-a37a-9282f92d6eef</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/bug fixing/g5</testCaseId>
+        <guid>184d2034-5d20-4e23-af83-9616b0f1feaf</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/New test case (11)</testCaseId>
+        <guid>61da2a65-fd16-41dc-8bf9-a7727827b833</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/leo</testCaseId>
+        <guid>86ea4f57-eed5-4d7f-89c4-8be1107cf630</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/regression/regression 1/regression 5</testCaseId>
+        <guid>ffb7cb2e-d833-4169-a1cb-bcf68b59e899</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/regression/4.4/regression 4</testCaseId>
+        <guid>a9534bbd-833c-4a29-8700-2ae6b1fd47da</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/regression/Regression 3</testCaseId>
+        <guid>c560cb91-8df8-46da-9521-d7ed1521f75c</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/test-folder/G5 Test Case</testCaseId>
+        <guid>31235d3c-e240-4a8e-a2dd-37229570351b</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/test-folder/New test case 1</testCaseId>
+        <guid>fc8b521f-3348-41ec-a244-e76ef0d8046a</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/test-folder/Test Case 4</testCaseId>
+        <guid>15e38b8a-d709-469c-993f-6bce370db9d0</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/Draft Test From Turing Team</testCaseId>
+        <guid>84399990-c3c7-4a52-a5f7-d99b8fa3e3e2</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/Verify the user can login</testCaseId>
+        <guid>a90b689e-05aa-4eee-a9e0-4348008746a9</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/bug fixing/Lorem Ipsum is simply dummied text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book</testCaseId>
+        <guid>cfadc1ff-7877-4862-b490-1e406d89ca60</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/bug fixing/draft test 1</testCaseId>
+        <guid>beed5bb3-f633-40c2-aeee-0227f66961d1</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/bug fixing/g5</testCaseId>
+        <guid>c47f5614-4f88-4df9-8868-31bd6ae7d53d</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/New test case (11)</testCaseId>
+        <guid>a8fbd64f-9b89-4173-941b-7541a1244112</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/leo</testCaseId>
+        <guid>1218d6e3-6ff4-4563-884e-10c83edcef96</guid>
     </testCaseLink>
 </TestSuiteEntity>
