1ff30a9c2ec31ca768f4cc79561d46a95f39b877
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-04-26 16:34:54 +0700

    first commit

---
+++ test-suites/cloud studio/ts.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>ts</name>
+    <testSuiteGuid>8d6e7c67-763b-4a95-9747-006bb2a4e12d</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/regression/regression 1/regression 5</testCaseId>
+        <guid>3f72c07f-81b1-4022-a204-ce766ce73238</guid>
+    </testCaseLink>
+</TestSuiteEntity>
