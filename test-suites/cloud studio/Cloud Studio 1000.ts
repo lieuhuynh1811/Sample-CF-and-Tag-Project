11e5c27f8a30318c2deef719fc39d2973a078b8c
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-04-26 15:31:11 +0700

    commit

---
+++ test-suites/cloud studio/Cloud Studio 1000.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>Cloud Studio 1000</name>
+    <testSuiteGuid>67c3aeab-90d9-4814-8bba-16d96564ebfb</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/bug fixing/Lorem Ipsum is simply dummied text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book</testCaseId>
+        <guid>3f0c76e5-ef37-4235-96f8-f8422971d892</guid>
+    </testCaseLink>
+</TestSuiteEntity>
