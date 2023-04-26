e9f98a4a2ec8b7e2a82805cbeea32877e7595ed4
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-04-26 17:27:25 +0700

    Katalon: Update test suite test suite 08 in test-suites/lieu-huynh

--- test-suites/lieu-huynh/test suite 08.ts
+++ test-suites/lieu-huynh/test suite 08.ts
@@ -1,13 +1,53 @@
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <TestSuiteEntity>
     <name>test suite 08</name>
-    <testSuiteGuid>336ba0c8-4bfd-4b1f-878e-e35516fa7ba2</testSuiteGuid>
+    <testSuiteGuid>26fefef7-b5a0-4460-a14e-73c4bda4384f</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/regression/regression 1/regression 5</testCaseId>
+        <guid>ed1d8b9b-8547-475b-b810-c785c6f20664</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/regression/Regression 3</testCaseId>
+        <guid>1edf6d0f-85d7-472e-9980-fe7d23c513ea</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/test-folder/G5 Test Case</testCaseId>
+        <guid>4c7fbc7d-8bc4-4edc-ad13-75488ff4501f</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/test-folder/New test case 1</testCaseId>
+        <guid>4d00caf6-307d-4555-905d-5e3fd601efee</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/test-folder/Test Case 4</testCaseId>
+        <guid>f06a5644-2814-4451-ab74-fa8322c827b3</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/Draft Test From Turing Team</testCaseId>
+        <guid>e6509584-3623-45b9-9d81-658808828014</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/Verify the user can login</testCaseId>
+        <guid>f82a7555-73f9-4ba4-8721-6bda945719e3</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/bug fixing/Lorem Ipsum is simply dummied text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book</testCaseId>
+        <guid>10bfb2ab-533b-4007-9df6-52331fdefae2</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/bug fixing/draft test 1</testCaseId>
+        <guid>91b17670-2b4d-4da5-a6af-2b494f93f1da</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/bug fixing/g5</testCaseId>
+        <guid>ec44096f-8a93-46d2-9ddb-b2c0d17cc7b2</guid>
+    </testCaseLink>
     <testCaseLink>
         <testCaseId>test-cases/New test case (11)</testCaseId>
-        <guid>060bfedc-b8c6-4b21-bff7-a2e8f6ba1a37</guid>
+        <guid>8c395f22-f32d-42a0-b562-26e6470f6154</guid>
     </testCaseLink>
     <testCaseLink>
         <testCaseId>test-cases/leo</testCaseId>
-        <guid>6c068de1-97f2-4de9-b32c-0e445058662e</guid>
+        <guid>e181e716-d45f-4200-bcf1-246e4bd82ad7</guid>
     </testCaseLink>
 </TestSuiteEntity>
