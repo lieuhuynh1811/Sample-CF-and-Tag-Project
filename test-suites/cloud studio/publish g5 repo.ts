b76ad1becf2ef012f2d0b5691ae703abde202130
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-05-09 14:48:46 +0700

    mai á

---
+++ test-suites/cloud studio/publish g5 repo.ts
@@ -0,0 +1,69 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>publish g5 repo</name>
+    <testSuiteGuid>fdb91a27-7452-4927-a421-00e80a262303</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/regression/regression 1/regression 5</testCaseId>
+        <guid>e42666ae-7397-4203-8356-9f1cfc96e7b7</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/regression/4.4/regression 4</testCaseId>
+        <guid>9c5317f0-f5e1-4821-b623-6fa79fbd68a5</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/regression/Regression 3</testCaseId>
+        <guid>d7fe1409-e09b-44f4-8318-ee15647d2410</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/read and write permission/no permission read and write</testCaseId>
+        <guid>a257c39a-c1a7-4260-bd82-654e3ec80741</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/read and write permission/publish g5 tc to git repo</testCaseId>
+        <guid>906eda06-40b0-4274-b74d-88f5f67110e7</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/test-folder/G5 Test Case</testCaseId>
+        <guid>045dfe06-1044-4c08-8449-5d50f71bb5b0</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/test-folder/New test case 1</testCaseId>
+        <guid>fd1b9f8b-6c5b-4fed-994a-b31d46d5782c</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/test-folder/Test Case 4</testCaseId>
+        <guid>b12bbc31-2b95-4f36-a0d7-f9a62728d2f8</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/Draft Test From Turing Team</testCaseId>
+        <guid>384f2a3f-01bc-466a-b580-5b67adc50ace</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/Verify the user can login</testCaseId>
+        <guid>53402458-ce5c-4253-a8a2-2f051f613b40</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/commit/New test case</testCaseId>
+        <guid>90a12de8-4541-4841-9bc9-6ad27b3751be</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/bug fixing/New test case (1)</testCaseId>
+        <guid>fffdf9e4-7085-4cbe-9621-c69a2e3acb14</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/bug fixing/draft test 1</testCaseId>
+        <guid>0ecd8536-0545-45b3-9652-bf53506c891c</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/bug fixing/g5</testCaseId>
+        <guid>a3e23446-ada0-4610-8608-0ff39e5cc93e</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/New test case (11)</testCaseId>
+        <guid>d4b7506a-5488-4dc5-8a32-8a5dfc242eaa</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/leo</testCaseId>
+        <guid>b30479d4-776f-4040-b8a9-6123e48bda2b</guid>
+    </testCaseLink>
+</TestSuiteEntity>
