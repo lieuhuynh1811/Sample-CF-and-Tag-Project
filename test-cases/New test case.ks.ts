56f5cb1dd8651d3f70995956abad0cdeb435906c
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-05-26 14:37:21 +0700

    leo

---
+++ test-cases/New test case.ks.ts
@@ -0,0 +1,39 @@
+import katalon from "@katalon-studio/cloud-test";
+
+export default katalon.testCase("New Test Case", async ({ local, web }) => {
+  await new Promise((r) => setTimeout(r, 0));
+  await web.openBrowser("https://katalon-demo-cura.herokuapp.com");
+  await web.click({
+    type: "Web",
+    id: "876346c8-1dad-4b61-a96d-d09aea34976f",
+    attributes: {
+      id: "btn-make-appointment",
+      href: "./profile.php#login",
+      class: "btn btn-dark btn-lg",
+    },
+    childIndex: 4,
+    hashes: { "md5.v1": "3caec008087ccb7a1cb3fc698fbf83c2" },
+    name: "a - Make Appointment",
+    selectors: [
+      {
+        id: "941fcca3-b303-4b33-a5cc-de1a2315f9eb",
+        type: "CSS",
+        value: "#btn-make-appointment",
+        isDefault: true,
+      },
+      {
+        id: "bb255d3f-5d37-43f1-8472-3f6637f79ce8",
+        type: "Attribute",
+        value: null,
+        isDefault: true,
+      },
+    ],
+    selectorType: "CSS",
+    tag: "a",
+    text: "Make Appointment",
+    pageUrl: "https://katalon-demo-cura.herokuapp.com/",
+    parentIframe: null,
+    shadowRoot: null,
+  });
+  await web.closeBrowser();
+});
