950fb36b3a2664014594d0bc7d389821e4b38504
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-07-14 16:23:36 +0700

    leo

---
+++ test-suites/G5.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>G5</name>
+    <testSuiteGuid>b2b184f1-f522-4bc5-9ff8-82aecf3f8be4</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/test-folder/G5 Test Case</testCaseId>
+        <guid>a7238315-02bb-4b12-9b9a-aba97235bc76</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/test-folder/Test Case 4</testCaseId>
+        <guid>94f977ef-c291-4f02-a611-393df0514748</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/Draft Test From Turing Team</testCaseId>
+        <guid>e22177aa-ae44-429a-b788-b533bf4f7f52</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/Verify the user can login</testCaseId>
+        <guid>3b060dd9-b0a3-4a4a-84e3-20e6f8b65a6d</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/New test case (11)</testCaseId>
+        <guid>8741d675-2e2c-4a71-89e2-936b7611f81a</guid>
+    </testCaseLink>
+</TestSuiteEntity>
