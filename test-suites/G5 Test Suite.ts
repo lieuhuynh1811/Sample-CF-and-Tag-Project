48de2c64e198ffbf3fa3b0b5796052f0f853c869
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-08-16 17:02:48 +0700

    leo

---
+++ test-suites/G5 Test Suite.ts
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>G5 Test Suite</name>
+    <testSuiteGuid>8f11392d-d61d-4b9e-b196-fba4c6294cd3</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/turn-off-flag/Katalon Cloud Turn Off flag</testCaseId>
+        <guid>cb63a8b1-268d-469c-bf93-c73f88eb7716</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/regression/regression 1/regression 5</testCaseId>
+        <guid>03b72ef3-171e-4e11-8534-a130350b227b</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/regression/4.4/regression 4</testCaseId>
+        <guid>9f7ce87a-819f-4b5b-aa8a-37b777e0efe3</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/regression/Regression 3</testCaseId>
+        <guid>62d603a1-5e60-4275-9dd2-14e6847e1df8</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/read and write permission/no permission read and write</testCaseId>
+        <guid>c09003ea-3a38-4fc5-b68b-f4dce370664c</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/read and write permission/publish g5 tc to git repo</testCaseId>
+        <guid>77bd6d31-bf86-484a-a3ee-12e57ec29d1b</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuhuynh/New test case (1)</testCaseId>
+        <guid>2f686496-2af3-470f-9885-63e96fd97726</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/test-folder/G5 Test Case</testCaseId>
+        <guid>87b41523-d112-43bc-88b4-6e0cd32d9ae9</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/test-folder/New test case 1</testCaseId>
+        <guid>45f994be-568f-4afb-9016-8fdfe3a9ded4</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/test-folder/Test Case 4</testCaseId>
+        <guid>8e109c58-7b29-460e-9d6d-b03acdc4e748</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/Draft Test From Turing Team</testCaseId>
+        <guid>10bdc741-9c2c-41d3-bdb8-4bd42fd4a824</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/lieuht96/Verify the user can login</testCaseId>
+        <guid>28e12b77-ec94-4a84-8c44-1ad5e24ea362</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/commit/New test case</testCaseId>
+        <guid>56194749-9480-4268-9d93-bc479400877e</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/bug fixing/Lorem Ipsum is simply dummied text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book</testCaseId>
+        <guid>bbca7302-4da3-4090-8107-dbc4959ffc1c</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/bug fixing/New test case (1)</testCaseId>
+        <guid>b3b6b967-5fac-44eb-85a8-b0ecc5d70541</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/bug fixing/draft test 1</testCaseId>
+        <guid>2f894abd-dd1a-405a-ac9e-a6c6e7d1276d</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/bug fixing/g5</testCaseId>
+        <guid>b6d8c37f-d902-40b9-a5a3-c6bb90a097c3</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/New test case</testCaseId>
+        <guid>6aa57f38-8e9c-4b51-b489-49d1910ebd86</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/New test case (11)</testCaseId>
+        <guid>0b9a1e24-45ca-4f80-a99f-c0439c591ce0</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/leo</testCaseId>
+        <guid>495e0dd1-99ce-46b6-a2cc-796db28531ed</guid>
+    </testCaseLink>
+</TestSuiteEntity>
