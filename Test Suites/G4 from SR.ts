fa8bb59811e1d10ea11422a55095edf73ea74688
lieuhuynh1811 <47736070+lieuhuynh1811@users.noreply.github.com>
2023-08-16 15:42:40 +0700

    leoleo

---
+++ Test Suites/G4 from SR.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>G4 from SR</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>12b9b7b7-8992-4bab-b9b0-6973a7f729ad</testSuiteGuid>
+   <testCaseLink>
+      <guid>2904c9f8-388d-4713-9fa4-c139e1a93711</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/sample tc</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
